d7e0e5482394ed293768760056a7635bc2cd68b0
Rodolfo Saldivar <rodolfo.saldivar@udem.edu>
2018-03-10 20:44:24 -0600

    r

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ app.get('/', (req, res) =>
 	res.send({hola:"brgbrrbrg"});
 });
 
+
+if (process.env.NODE_ENV === 'production')
+{
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) =>
+	{
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 app.listen(process.env.PORT || 5000);
